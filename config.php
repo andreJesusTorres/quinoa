76fc8c744c133d3d9b0807eab3de000f4f4850cb
andreJesusTorres <andre.torres@outlook.com.ar>
2024-05-20 22:47:28 +0200

    Cambios hostinger

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define('server', 'localhost');
-define('usuario', 'root');
-define('clave', '');
-define('nombre', 'quinoa');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'u145872577_quinoa');
+define('DB_PASSWORD', 'y0X+sE1UQrn');
+define('DB_NAME', 'u145872577_quinoa');
 ?>
